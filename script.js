a704db29cd52573995a9ea376c473f48b06bf607
Amr Alaaeldin <80746926+amralaaeledin@users.noreply.github.com>
2021-10-04 02:05:05 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,115 @@
+const head = document.querySelectorAll("header .container nav ul .head");
+const headEle = [...head];
+const nav = document.querySelector("header .container nav");
+const header = document.querySelector("header");
+const group = document.querySelectorAll(
+  "header .container nav ul .head .group"
+);
+const groupEle = [...group];
+const openIcon = document.querySelector(".list-open");
+const closeIcon = document.querySelector(".list-close");
+
+// desktop
+// rest is close when outside
+headEle.forEach((ele) => {
+  ele.addEventListener("click", () => {
+    if (!ele.classList.contains("active")) {
+      headEle.forEach((ele) => {
+        ele.classList.remove("active");
+      });
+      ele.classList.add("active");
+    } else if (ele.classList.contains("active")) {
+      ele.classList.remove("active");
+    }
+  });
+});
+
+// mobile
+// rest is close when outside
+// search in classlist array or string about the class name
+// make closing [not sure]
+openIcon.addEventListener("click", () => {
+  if (header.className != "active" && nav.className != "active") {
+    header.classList.add("active");
+    nav.classList.add("active");
+  }
+});
+
+headEle.forEach((ele, index) => {
+  ele.addEventListener("click", () => {
+    // here
+    if (
+      header.className == "active" &&
+      nav.className == "active" &&
+      !groupEle[index].classList.contains("active")
+    ) {
+      groupEle.forEach((ele) => {
+        ele.classList.remove("active");
+      });
+      groupEle[index].classList.add("active");
+    } else if (
+      header.className == "active" &&
+      nav.className == "active" &&
+      groupEle[index].classList.contains("active")
+    ) {
+      groupEle[index].classList.remove("active");
+    }
+  });
+});
+
+function close() {
+  header.classList.remove("active");
+  nav.classList.remove("active");
+  headEle.forEach((ele) => {
+    ele.classList.remove("active");
+  });
+  groupEle.forEach((ele) => {
+    ele.classList.remove("active");
+  });
+}
+closeIcon.addEventListener("click", () => {
+  if (header.className == "active" && nav.className == "active") {
+    close();
+  }
+});
+
+// close group when click outside (mobile)
+nav.addEventListener("click", (e) => {
+  if (e.target.tagName != "UL" && e.target.tagName != "LI") {
+    console.log("worked now!");
+    headEle.forEach((ele) => {
+      ele.classList.remove("active");
+    });
+    groupEle.forEach((ele) => {
+      ele.classList.remove("active");
+    });
+  }
+});
+
+// close all when click outside (mobile)
+document.addEventListener("click", (e) => {
+  if (
+    !e.target.classList.contains("group") &&
+    !e.target.classList.contains("active") &&
+    !e.target.classList.contains("list-open") &&
+    !e.target.classList.contains("btns") &&
+    !e.target.classList.contains("btn") &&
+    !e.target.classList.contains("divider") &&
+    e.target.tagName != "LI"
+  ) {
+    close();
+  }
+});
+
+// close all when click outside (desktop)
+document.addEventListener("click", (e) => {
+  if (
+    !e.target.classList.contains("group") &&
+    !e.target.classList.contains("active")
+  ) {
+    // close all when click outside (desktop)
+    head.forEach((ele) => {
+      ele.classList.remove("active");
+    });
+  }
+});
